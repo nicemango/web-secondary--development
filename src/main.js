c19248e72291c40a471b3e71dfe384f04bc33eeb
jiangsixian <jiangsixian@njsdata.com>
2022-05-18 00:32:20 +0800

    vue版本应用二开初始化

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import Vue from "vue";
+import App from "./App.vue";
+// 按需引入组件，引入方式见https://element.eleme.cn/#/zh-CN/component/quickstart#an-xu-yin-ru
+import { Input, Select, Option, Button, Avatar } from "element-ui";
+
+Vue.config.productionTip = false;
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Button);
+Vue.use(Avatar);
+
+// import * as appService from "@njsdata/app-sdk";
+
+if (process.env.NODE_ENV !== "production") {
+  // 添加 customConfig 进行测试
+  let customConfig = {
+    title: "数据构建",
+    desc: "无码化应用搭建，弹指间即完成数据从无到有到收集和使用",
+    url: "http://baidu.com",
+    imgUrl:
+      "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+  };
+
+  new Vue({
+    render: h => {
+      return <App customConfig={customConfig} />;
+    },
+  }).$mount("#app");
+} else {
+  if (!window.CUSTOM_PLUGIN) {
+    window.CUSTOM_PLUGIN = new Map();
+  }
+
+  window.CUSTOM_PLUGIN.set(
+    process.env.VUE_APP_CUSTOM_PLUGIN_ID,
+    (dom, props) => {
+      const customConfig = (props && props.customConfig) || {};
+      const component = new Vue({
+        render: h => <App customConfig={customConfig} info={props} />,
+      }).$mount();
+      if (dom.childNodes.length > 0) {
+        dom.removeChild(dom.childNodes[0]);
+      }
+      dom.appendChild(component.$el);
+    }
+  );
+}
