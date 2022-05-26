0c6c556a04f6db9cf71821f6d040fa1fb6f5080a
jiangsixian <jiangsixian@njsdata.com>
2022-05-26 15:54:55 +0800

    feat:教研过滤器面板代码提交

--- src/App.js
+++ src/App.js
@@ -1,97 +1,175 @@
-import React, { Component } from "react";
-import { Button, Card } from "antd";
-// import appService from "@njsdata/app-sdk";
-import "./app.less";
+import React, {Component} from "react";
+import {
+  Radio,
+  message,
+  Input,
+} from "antd";
+import {
+  FilterOutlined
+} from '@ant-design/icons';
+import "./app.less"
 
 export default class App extends Component {
+  state = {
+    first: true, filterContentShow: true,
+  }
+  filterDataArr = []
+  selected = {}
+
   componentDidMount() {
+    window.main = this;
+    this.setState({first: false})
+    this.filterToolbarContent.current.parentNode.height = this.filterToolbarContent.current.offsetHeight
     const events = [
       {
-        key: "jumpButton",
-        name: "跳转按钮",
-        payload: [],
-      },
+        key: "selectedValueChange", name: "过滤条件变化", payload: [
+          {
+            key: "selectedValue", name: "过滤条件", dataType: "string"
+          }
+        ],
+      }, {
+        key: "searchValueChange", name: "搜索条件变化", payload: [
+          {
+            key: "searchValue", name: "搜索条件", dataType: "string"
+          }
+        ],
+      }
     ];
+    const actions = []
+    this.props?.customConfig?.componentId && window.componentCenter?.register(this.props?.customConfig?.componentId, "", this, {
+      events, actions
+    });
+  }
 
-    const actions = [
-      {
-        key: "messageSuccess",
-        name: "刷新页面",
-      },
-    ];
-    this.props?.customConfig?.componentId &&
-      window.componentCenter?.register(
-        this.props?.customConfig?.componentId,
-        "",
-        this,
-        {
-          events,
-          actions,
+  filterToolbarContent = React.createRef()
+
+  searchValueChange = (val) => {
+    window.eventCenter?.triggerEvent(this.props?.customConfig?.componentId, "searchValueChange", {searchValue: val})
+  }
+  selectedValueChange = (val) => {
+    let selected = []
+    let selectedVal = val?.target?.value
+    console.log(val?.target?.value);
+    this.filterDataArr.forEach((filterRow) => {
+      filterRow.filterData.forEach((item) => {
+        if (item === selectedVal) {
+          this.selected[filterRow.filterName] = selectedVal
         }
-      );
-    // window.componentCenter.registerTriggerForType(
-    //   this.props.componentId,
-    //   "process",
-    //   this,
-    //   {
-    //     events,
-    //     actions,
-    //   }
-    // );
+      })
+    })
+    for (let selectedKey in this.selected) {
+      selected.push(this.selected[selectedKey])
+    }
+    console.log(selected);
+    window.eventCenter?.triggerEvent(this.props?.customConfig?.componentId, "selectedValueChange", {selectedValue: selected.join(",")})
   }
 
-  goToStudy = () => {
-    this.props?.customConfig?.url && window.open(this.props?.customConfig?.url);
-  };
-  // getData = () => {
-  // console.log(appService.getMenuData(), "菜单");
-  // console.log(appService.getPageData(), "页面");
-  // console.log(appService.getVariable(), "变量");
-  // };
-
-  do_EventCenter_messageSuccess() {
-    window.location.reload();
+  handleFilterIconClick = () => {
+    this.setState({filterContentShow: !this.state.filterContentShow})
   }
 
-  // 逻辑控制用，不可删
   Event_Center_getName() {
-    return "应用二开测试";
+    return "filterData";
   }
 
   render() {
-    const { customConfig } = this.props;
-    const { title, desc, url, color, componentId } = customConfig || {};
-    return (
-      <Card
-        className="infoCard"
-        bordered={false}
-        title={<span className="card-title">{title || "数据构建"}</span>}
-        extra={
-          <Button
-            ghost
-            onClick={() => {
-              window?.eventCenter?.triggerEvent(componentId, "jumpButton");
-            }}
-          >
-            跳转
-          </Button>
-        }
+    const {customConfig} = this.props;
+    let {filterData, title, buttonWidth, filterIconRight} = customConfig || {};
+    let buttonWidthString;
+    let filterIconRightString;
+    buttonWidthString = buttonWidth ? `${buttonWidth}px` : "100px";
+
+    filterIconRightString = filterIconRight ? `${filterIconRight}px` : "20px"
+    let filterDataArr
+    try {
+      filterDataArr = JSON.parse(filterData)
+
+    } catch (e) {
+      message.error("输入的数据格式有误")
+    }
+    this.filterDataArr = filterDataArr
+    this.filterDataArr.forEach((filterRow) => {
+      this.selected[filterRow.filterName] = filterRow.defaultValue
+    })
+    return (<>
+      <div className="filterToolbarHeader">
+        <div
+          className="filterToolbarContentRight"
+          onClick={this.handleFilterIconClick}
+          style={{right: filterIconRightString}}
+        >
+          <FilterOutlined/>
+        </div>
+      </div>
+      <div
+        className="shrink"
         style={{
-          background: color || "#0454f2",
+          overflow: "hidden",
+          height: !this.state.filterContentShow ? "80px" : this.state.first
+                                                        ? "80px"
+                                                        : this.filterToolbarContent.current.offsetHeight + "px",
+          transition: "0.5s"
         }}
       >
-        <p className="card-desc">
-          {desc || "无码化应用搭建，弹指间即完成数据从无到有到收集和使用"}
-        </p>
-        <Button
-          ghost
-          onClick={() => {
-            window.open(url || "https://www.sdata1010.cn/");
-          }}
+        <div
+          className="filterToolbarContent" ref={this.filterToolbarContent}
         >
-          前往
-        </Button>
-      </Card>
-    );
+          <div className="filterToolbarContentLeft">
+            {filterDataArr.map((filterRow, filterRowIndex) => (<div
+              className="filter-row" key={filterRowIndex} style={{
+              marginLeft: "40px", display: "flex", justifyContent: "left"
+            }}
+            >
+              <div
+                className="filter-label"
+                style={{
+                  display: "inline-block",
+                  width: "85px",
+                  textAlign: "center",
+                  fontSize: "18px",
+                  fontFamily: "PingFang SC",
+                  marginTop: "43px"
+
+                }}
+              >{filterRow.filterName}</div>
+              <Radio.Group
+                onChange={this.selectedValueChange}
+                style={{marginLeft: "40px", width: "calc(100% - 150px)"}}
+                defaultValue={filterRow.defaultValue}
+              >
+                {filterRow.filterData.map((filterItem, filterItemIndex) => (<>
+                  <Radio.Button
+                    style={{
+                      borderRadius: "6px",
+                      border: "1px solid #999",
+                      minWidth: buttonWidthString,
+                      color: "#999",
+                      height: "32px",
+                      cursor: "pointer",
+                      textAlignlign: "center",
+                      fontSize: "14px",
+                      marginRight: "17px",
+                      boxSizing: "border-box",
+                      marginTop: "43px"
+                    }}
+                    value={filterItem}
+                    key={filterItemIndex}
+                  >
+                    {filterItem}
+                  </Radio.Button>
+                </>))}
+              </Radio.Group>
+            </div>))}
+          </div>
+          <div className="filterToolbarContentRight">
+            <Input.Search
+              onSearch={this.searchValueChange}
+              placeholder="搜索感兴趣的应用名称"
+              style={{display: "inline-block", width: "260px",}}
+            />
+          </div>
+        </div>
+      </div>
+    </>);
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
 /**
  * 生产打包时为了减少体积，不引入 antd.css (可节约 2.5M左右的包体积)
  * 生产包是当做 onemind 主站插件使用的，页面里已经有一份 ant.css 了，所以这里可以省去
@@ -12,14 +13,64 @@ if (process.env.NODE_ENV !== "production") {
   require("antd/dist/antd.css");
   // 添加 customConfig 进行测试
   let customConfig = {
-    title: "数据构建",
-    desc: "无码化应用搭建，弹指间即完成数据从无到有到收集和使用",
-    url: "https://www.sdata1010.cn/",
-    imgUrl:
-      "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
-  };
+    filterData: JSON.stringify([
+      {
+        filterName: "课程系列",
+        filterData: [
+          "全部",
+          "快速入门篇",
+          "基础配置篇",
+          "进阶设计篇",
+          "业务场景篇",
+          "功能详解篇",
+          "技术专题篇",
+          "版本新特性"
+        ],
+        defaultValue:"全部"
+      },
+      {
+        filterName: "难度等级",
+        filterData: [
+          "全部",
+          "初级",
+          "中级",
+          "高级"
+        ],
+        defaultValue:"全部"
+      },
+      {
+        filterName: "产品应用",
+        filterData: [
+          "全部",
+          "了解产品",
+          "应用设计",
+          "逻辑配置",
+          "调查问卷",
+          "填报设计",
+          "业务流程",
+          "数据分析仪",
+          "数据图书馆",
+          "数据大屏",
+          "二次开发",
+          "视频源",
+          "数据交换机",
+          "数据服务",
+          "数据源",
+          "数据连接器",
+          "数据文档",
+          "通讯录",
+          "系统管理",
+          "视图列表"
+        ],
+        defaultValue:"全部"
+      }
+    ]),
+    title: "测试",
+    buttonWidth:100,
+    filterIconRight:50
+  }
   ReactDOM.render(
-    <App customConfig={customConfig} />,
+    <App customConfig={customConfig}/>,
     document.getElementById("root")
   );
 } else {
