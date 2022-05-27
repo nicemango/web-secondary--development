a54d438bf7b6ea20894e97c11c447475e1f2f74d
jiangsixian <jiangsixian@njsdata.com>
2022-05-27 14:58:21 +0800

    fix:修改vue渲染方式及gitignore文件

--- src/main.js
+++ src/main.js
@@ -35,14 +35,14 @@ if (process.env.NODE_ENV !== "production") {
   window.CUSTOM_PLUGIN.set(
     process.env.VUE_APP_CUSTOM_PLUGIN_ID,
     (dom, props) => {
-      const customConfig = (props && props.customConfig) || {};
-      const component = new Vue({
-        render: h => <App customConfig={customConfig} info={props} />,
-      }).$mount();
       if (dom.childNodes.length > 0) {
         dom.removeChild(dom.childNodes[0]);
       }
-      dom.appendChild(component.$el);
+      const div = document.createElement("div");
+      dom.appendChild(div);
+      new Vue({
+        render: h => <App {...{ props }} />,
+      }).$mount(div);
     }
   );
 }
