8143d6b75beaf6aaa9cf9c47f0f36d26e944e694
jiangsixian <jiangsixian@njsdata.com>
2022-07-14 15:56:41 +0800

    feat:react版本填报列表行内按钮模板修改

---
+++ dumi/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
---
+++ dumi/.umi/dumi/demos/index.ts
@@ -0,0 +1,43 @@
+// @ts-nocheck
+import React from 'react';
+import { dynamic } from 'dumi';
+import rawCode1 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/design.jsx?dumi-raw-code';
+import rawCode2 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/src/App.jsx?dumi-raw-code';
+import rawCode3 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/src/components/list/index.js?dumi-raw-code';
+import rawCode4 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/src/api/asset.js?dumi-raw-code';
+import rawCode5 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/src/api/request.js?dumi-raw-code';
+import rawCode6 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/src/components/list/index.less?dumi-raw-code';
+import rawCode7 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/src/components/designConfiguration/index.js?dumi-raw-code';
+import rawCode8 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/mockComponentCenter.ts?dumi-raw-code';
+import rawCode9 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/designConfiguration.jsx?dumi-raw-code';
+import rawCode10 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/formMain.jsx?dumi-raw-code';
+import rawCode11 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/formChild.jsx?dumi-raw-code';
+import rawCode12 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/table.jsx?dumi-raw-code';
+import rawCode13 from '!!dumi-raw-code-loader!/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/detail.jsx?dumi-raw-code';
+
+export default {
+  'list-custom-btn-design': {
+    component: (require('/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/design.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode1},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/api/asset.js":{"import":"./../../api/asset","content":rawCode4},"src/api/request.js":{"import":"./request","content":rawCode5},"src/components/list/index.less":{"import":"./index.less","content":rawCode6},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode7},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode8}},"dependencies":{"react":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"axios":{"version":"0.21.4"},"querystringify":{"version":"2.2.0"}},"identifier":"list-custom-btn-design"},
+  },
+  'list-custom-btn-designconfiguration': {
+    component: (require('/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/designConfiguration.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode9},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/api/asset.js":{"import":"./../../api/asset","content":rawCode4},"src/api/request.js":{"import":"./request","content":rawCode5},"src/components/list/index.less":{"import":"./index.less","content":rawCode6},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode7},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode8}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"axios":{"version":"0.21.4"},"querystringify":{"version":"2.2.0"}},"identifier":"list-custom-btn-designconfiguration"},
+  },
+  'list-custom-btn-formmain': {
+    component: (require('/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/formMain.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode10},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/api/asset.js":{"import":"./../../api/asset","content":rawCode4},"src/api/request.js":{"import":"./request","content":rawCode5},"src/components/list/index.less":{"import":"./index.less","content":rawCode6},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode7},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode8}},"dependencies":{"react":{"version":">=16.9.0"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"axios":{"version":"0.21.4"},"querystringify":{"version":"2.2.0"}},"identifier":"list-custom-btn-formmain"},
+  },
+  'list-custom-btn-formchild': {
+    component: (require('/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/formChild.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode11},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/api/asset.js":{"import":"./../../api/asset","content":rawCode4},"src/api/request.js":{"import":"./request","content":rawCode5},"src/components/list/index.less":{"import":"./index.less","content":rawCode6},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode7},"mockComponentCenter.ts":{"import":"./mockComponentCenter","content":rawCode8}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"axios":{"version":"0.21.4"},"querystringify":{"version":"2.2.0"}},"identifier":"list-custom-btn-formchild"},
+  },
+  'list-custom-btn-table': {
+    component: (require('/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/table.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode12},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/api/asset.js":{"import":"./../../api/asset","content":rawCode4},"src/api/request.js":{"import":"./request","content":rawCode5},"src/components/list/index.less":{"import":"./index.less","content":rawCode6},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode7}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"axios":{"version":"0.21.4"},"querystringify":{"version":"2.2.0"}},"identifier":"list-custom-btn-table"},
+  },
+  'list-custom-btn-detail': {
+    component: (require('/Users/wangjinbao/code/sdata-custome-plugins/list-custom-btn/list-custom-btn/demo/detail.jsx')).default,
+    previewerProps: {"sources":{"_":{"jsx":rawCode13},"src/App.jsx":{"import":"../src/App","content":rawCode2},"src/components/list/index.js":{"import":"./components/list","content":rawCode3},"src/api/asset.js":{"import":"./../../api/asset","content":rawCode4},"src/api/request.js":{"import":"./request","content":rawCode5},"src/components/list/index.less":{"import":"./index.less","content":rawCode6},"src/components/designConfiguration/index.js":{"import":"./components/designConfiguration","content":rawCode7}},"dependencies":{"react":{"version":">=16.9.0"},"prop-types":{"version":"15.8.1"},"antd":{"version":"4.16.6","css":"antd/dist/antd.css"},"react-dom":{"version":">=16.9.0"},"axios":{"version":"0.21.4"},"querystringify":{"version":"2.2.0"}},"identifier":"list-custom-btn-detail"},
+  },
+};
