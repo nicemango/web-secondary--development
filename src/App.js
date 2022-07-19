e76b264f40e4a9bb3b4a52dc6eb315e1b73dfc53
拾肆 <71803979+1977953215@users.noreply.github.com>
2022-07-19 10:17:31 +0800

    feat:达远-仪表盘 (#115)

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+
+import Highcharts from 'highcharts/highstock';
+import HighchartsMore from 'highcharts/highcharts-more';
+
+HighchartsMore(Highcharts)
+
+const events = [];
+const actions = [];
+
+export default class App extends Component {
+  state = {};
+
+  constructor(props) {
+    super(props);
+
+    let external = props.options?.externalVariables ? props.options.externalVariables : {}
+    
+    this.from1 = external.区间一 || '0-40'
+    this.from2 = external.区间二 || '40-160'
+    this.from3 = external.区间三 || '160-200'
+    this.fromMax = external.区间最大值 || '200'
+    this.fromTitle = external.仪表盘标题 || 'km/h'
+    this.fromValue = external.仪表盘数值 || '80'
+
+  }
+
+  componentDidMount() {
+    const { updateProcess, componentId } = this.props;
+
+    window?.componentCenter?.register(componentId, "comp", this, {
+      events,
+      actions,
+    });
+    updateProcess && updateProcess();
+
+    this.Event_Center_getName = () => {
+      return "仪表盘";
+    };
+
+    this.initEcharts()
+  }
+
+  initEcharts() {
+    Highcharts.chart(this.refs['gaugeEcharts'],{
+      credits: { enabled: false },
+      chart: { type: 'gauge' },
+      title: { text: '' },
+      pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+          backgroundColor: { linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 }, stops: [ [0, '#FFF'], [1, '#333'] ] },
+          borderWidth: 0,
+          outerRadius: '109%'
+        }, {
+          backgroundColor: { linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 }, stops: [ [0, '#333'], [1, '#FFF'] ] },
+          borderWidth: 1,
+          outerRadius: '107%'
+        }, {}, {
+          backgroundColor: '#DDD',
+          borderWidth: 0,
+          outerRadius: '105%',
+          innerRadius: '103%'
+        }]
+      },
+      yAxis: {
+        min: 0,
+        max: this.fromMax,
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: { step: 2, rotation: 'auto' },
+        title: { text: this.fromTitle },
+        plotBands: [
+          {
+            from: Number(this.from1.split('-')[0]),
+            to: Number(this.from1.split('-')[1]),
+            color: '#55BF3B' // green
+          }, 
+          {
+            from: Number(this.from2.split('-')[0]),
+            to: Number(this.from2.split('-')[1]),
+            color: '#DDDF0D' // yellow
+          }, 
+          {
+            from: Number(this.from3.split('-')[0]),
+            to: Number(this.from3.split('-')[1]),
+            color: '#DF5353' // red
+          }
+        ]
+      },
+      series: [
+        {
+          data: [Number(this.fromValue)],
+        }
+      ]
+    });
+  }
+
+  render() {
+    return(
+      <div ref="gaugeEcharts" style={{ width: '100%', height: '100%'}}></div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: wangchengyuan
+ * @Email: wangchengyuan@njsdata.com
+ * @LastEditors: Do not edit
+ * @Date: 2021-04-06 09:58:34
+ * @LastEditTime: 2021-10-19 17:43:29
+ * @FilePath: \om-app-plugin-react-boilerplate\src\index.js
+ * @Description: 请描述文件作用
+ */
+/* 可以考虑在发布的代码里移除这个css */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+/**
+ * 生产打包时为了减少体积，不引入 antd.css (可节约 2.5M左右的包体积)
+ * 生产包是当做 onemind 主站插件使用的，页面里已经有一份 ant.css 了，所以这里可以省去
+ * 从功能通过 webpack.IgnorePlugin 插件实现，如果想要打入此 css，请在 webpack 配置中做修改。
+ */
+if (process.env.NODE_ENV !== "production") {
+  require("antd/dist/antd.css");
+  const dataSource = [
+    ["标题一", "标题二", "标题三"],
+    ["标题一数据一", "标题二数据一", "标题三数据一"],
+    ["标题一数据二", "标题二数据二", "标题三数据二"],
+    ["标题一数据三", "标题二数据三", "标题三数据三"],
+  ];
+  const options = {
+    externalVariables: {
+      headerBGColor: "#000",
+    },
+  };
+  ReactDOM.render(
+    <App dataSource={dataSource} options={options} />,
+    document.getElementById("root")
+  );
+} else {
+  if (!window.CUSTOM_PLUGIN) {
+    window.CUSTOM_PLUGIN = new Map();
+  }
+  window.CUSTOM_PLUGIN.set(process.env.CUSTOM_PLUGIN_ID, (dom, props) => {
+    ReactDOM.render(<App {...props} />, dom);
+  });
+}
